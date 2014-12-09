4561fa3992f48bd5475a089cd9a5717192f0c475
Budhram Gurung <budhram.gurung01@gmail.com>
2014-12-09 22:40:05 +0530

    Topic db migration

--- db/migrate/20141209170744_create_topics.rb
+++ db/migrate/20141209170744_create_topics.rb
@@ -1,8 +1,14 @@
 class CreateTopics < ActiveRecord::Migration
-  def change
+  def up
     create_table :topics do |t|
+      t.string :name
+      t.string :priority
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :topics
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208172302) do
+ActiveRecord::Schema.define(version: 20141209170744) do
+
+  create_table "topics", force: true do |t|
+    t.string   "name"
+    t.string   "priority"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", primary_key: "userid", force: true do |t|
     t.string   "useremployeeid", limit: 6
