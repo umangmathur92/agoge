8a90db835f3faf50aee7e6203e52890a14d4633b
Budhram Gurung <budhram.gurung01@gmail.com>
2014-12-09 22:50:17 +0530

    Seed data for topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
 # Seed data for Users
 
 User.delete_all
-
 1.upto(50).each do |num|
   User.create! do |user|
     user.useremployeeid = "V#{num}"
     user.username = "User #{num}"
     user.email = "user#{num}@abc.com"
     user.uid = "user_#{num}"
+  end
+end
+
+# topic priority
+PRIORITY = %w(urgent normal low)
+
+Topic.delete_all
+1.upto(20).each do |num|
+  Topic.create! do |topic|
+    topic.name = "Topic #{num}"
+    topic.priority = PRIORITY[num % 3]
   end
 end
