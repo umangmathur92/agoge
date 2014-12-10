6a7bdc69fe698ea184cfdfc6886b547fa6a63e7c
Budhram Gurung <budhram.gurung01@gmail.com>
2014-12-10 19:03:20 +0530

    Added description column for topics

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210132536) do
+ActiveRecord::Schema.define(version: 20141210133247) do
 
   create_table "topics", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "topics_users", force: true do |t|
