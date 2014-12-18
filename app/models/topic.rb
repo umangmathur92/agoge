10d3a19ba0e0280cc34c2fff67bbe86176d2e3cd
Budhram Gurung <budhram.gurung01@gmail.com>
2014-12-18 19:24:42 +0530

    Added methods to access join table attributes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,11 @@
 class Topic < ActiveRecord::Base
   has_many :topic_users
   has_many :users, through: :topic_users
+
+  %w(priority present created_at).each do |name|
+    define_method "#{name}_by_user" do |user_id|
+      topic_user = TopicUser.where(user_id: user_id).first
+      topic_user ? topic_user.send(name) : false
+    end
+  end
 end
