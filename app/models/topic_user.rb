50a29fdb81f63cabe8409665d21d685c7207a293
Budhram Gurung <budhram.gurung01@gmail.com>
2014-12-18 18:06:04 +0530

    Created has_many :through relationship between user and topic

---
+++ app/models/topic_user.rb
@@ -0,0 +1,4 @@
+class TopicUser < ActiveRecord::Base
+  belongs_to :topic
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :topic_users
+  has_many :topics, through: topic_users
 end
